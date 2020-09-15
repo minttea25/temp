40d72fde90f7764f0c03e9b4b96282a70f57347a
minttea25 <minttea.dev@gmail.com>
2020-09-15 23:53:31 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,9 @@
-class Test {
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        for (int i=0; i<3; i++) {
+            System.out.println(i);
+        }
+    }
 }
